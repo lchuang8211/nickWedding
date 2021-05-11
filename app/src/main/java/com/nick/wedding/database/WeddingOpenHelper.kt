20a9fa0a184be85fc21cc0e0a47415dc0cc19028
DESKTOP-OSNBSEK\Yellow <hlc>
2021-05-11 20:03:06 +0800

    新增 兌換資料表

--- app/src/main/java/com/nick/wedding/database/WeddingOpenHelper.kt
+++ app/src/main/java/com/nick/wedding/database/WeddingOpenHelper.kt
@@ -3,6 +3,7 @@ package com.nick.wedding.database
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import timber.log.Timber
 
 
 class WeddingOpenHelper(context: Context) : SQLiteOpenHelper(context, name, CursorFactory, version) {
@@ -10,22 +11,33 @@ class WeddingOpenHelper(context: Context) : SQLiteOpenHelper(context, name, Curs
     companion object {
         val name = "kkLove.db"
         val CursorFactory = null
-        val version = 1
+        val version = 2
     }
 
     val dateTable = "dateTable"
     val date = "date"
     val sign = "sign"
-    val table_date_sql = "CREATE TABLE if not exists $dateTable($date TEXT PRIMARY KEY NOT NULL, $sign BLOB NOT NULL)"
+    private val table_date_sql = "CREATE TABLE if not exists $dateTable($date TEXT PRIMARY KEY NOT NULL, $sign BLOB NOT NULL)"
+
+    val exchangeTable = "exchangeTable"
+    val category="category"
+    val title = "title"
+    val price = "price"
+    val seq = "seq"
+    val visibility = "visibility"
+    private val table_exchange_sql = "CREATE TABLE  if not exists $exchangeTable ( $category TEXT NOT NULL, $title TEXT NOT NULL, $price INTEGER NOT NULL, $seq INTEGER NOT NULL, $visibility INTEGER NOT NULL, PRIMARY KEY($category,$seq) )"
 
     //建置資料庫的前置動作
     override fun onCreate(db: SQLiteDatabase?) {
-        db!!.execSQL(table_date_sql)
-
+        db?.execSQL(table_date_sql)
+        db?.execSQL(table_exchange_sql)
     }
+
     //刪除及更新表格動作
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db!!.execSQL(table_date_sql)
+        Timber.tag("hlcDebug").d(" Version: $oldVersion -> $newVersion")
+        db?.execSQL(table_date_sql)
+        db?.execSQL(table_exchange_sql)
         onCreate(db)
     }
 }
